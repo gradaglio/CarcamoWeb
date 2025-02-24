8f941b7822aecc4a7d6e1b6086dbc0091bda69db
gradaglio <gradaglio@gmail.com>
2025-02-24 17:53:11 -0300

    creacion del proyecto

---
+++ src/layout/panel/Footer.js
@@ -0,0 +1,14 @@
+import {Box} from "@chakra-ui/react";
+
+
+const Footer = () => {
+    const currentYear = new Date().getFullYear();
+  
+    return (
+      <footer>
+        <Box bg="#001112">© {currentYear} Centro Psicoanalítico "Dr. Celes E. Cáramos - Filial Quilmes de APA</Box>
+      </footer>
+    );
+  };
+  
+  export default Footer;
